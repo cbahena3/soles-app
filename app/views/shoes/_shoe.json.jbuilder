b9dd5183212f3b93092d748199d2f5b76e0d1931
Cristian B <cris.bahena77@gmail.com>
2024-01-02 20:36:12 -0600

    Add index action

---
+++ app/views/shoes/_shoe.json.jbuilder
@@ -0,0 +1,5 @@
+json.name shoe.name
+json.brand shoe.brand
+json.size shoe.size
+json.price shoe.price
+json.image shoe.image
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-shoe = Shoe.create(name: "Tasman Slipper", brand: "UGG", size: " W", price: 110, image: "https://dms.deckers.com/ugg/image/upload/f_auto,q_auto,dpr_auto/w_1650/v1644928675/catalog/images/transparent/5955-CHE_1.png?_s=RAABAB0")
+# shoe = Shoe.create(name: "Tasman Slipper", brand: "UGG", size: " W", price: 110, image: "https://dms.deckers.com/ugg/image/upload/f_auto,q_auto,dpr_auto/w_1650/v1644928675/catalog/images/transparent/5955-CHE_1.png?_s=RAABAB0")
 
-shoe = Shoe.create(name: "Jordan 1 Mid", brand: "Nike", size: "9 M", price: 120, image: "https://i.ebayimg.com/images/g/vl4AAOSwM3Bk-fOJ/s-l1200.jpg" )
+# shoe = Shoe.create(name: "Jordan 1 Mid", brand: "Nike", size: "9 M", price: 120, image: "https://i.ebayimg.com/images/g/vl4AAOSwM3Bk-fOJ/s-l1200.jpg" )
 
-shoe = Shoe.create(name: "New Balance 550", brand: "New Balance", size: "8 M" , price: 98, image: "https://nb.scene7.com/is/image/NB/bbw550ww_nb_02_i?$pdpflexf2$&wid=440&hei=440")
+# shoe = Shoe.create(name: "New Balance 550", brand: "New Balance", size: "8 M" , price: 98, image: "https://nb.scene7.com/is/image/NB/bbw550ww_nb_02_i?$pdpflexf2$&wid=440&hei=440")
 
-shoe = Shoe.create(name: "Air Force 1 Low '07", brand: "Nike", size: "11 M" , price: 100, image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/82aa97ed-98bf-4b6f-9d0b-31a9f907077b/air-force-1-07-mens-shoes-jBrhbr.png")
+# shoe = Shoe.create(name: "Air Force 1 Low '07", brand: "Nike", size: "11 M" , price: 100, image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/82aa97ed-98bf-4b6f-9d0b-31a9f907077b/air-force-1-07-mens-shoes-jBrhbr.png")
 
-shoe = Shoe.create(name: "Nike SB Chron 2 Canvas", brand: "Nike", size: "8 M" , price: 65, image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/03bef457-7764-46a9-9719-6217cf66fbff/sb-chron-2-canvas-skate-shoes-VmcNLG.png" )
+# shoe = Shoe.create(name: "Nike SB Chron 2 Canvas", brand: "Nike", size: "8 M" , price: 65, image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/03bef457-7764-46a9-9719-6217cf66fbff/sb-chron-2-canvas-skate-shoes-VmcNLG.png" )
 
-shoe = Shoe.create(name: "Air Jordan 1 Retro High OG", brand: "Nike", size: "6.5 W" , price: 180, image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/8aa1b974-a557-4b86-90ea-f56dab700262/air-jordan-1-retro-high-og-womens-shoes-SsFGW3.png" )
+# shoe = Shoe.create(name: "Air Jordan 1 Retro High OG", brand: "Nike", size: "6.5 W" , price: 180, image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/8aa1b974-a557-4b86-90ea-f56dab700262/air-jordan-1-retro-high-og-womens-shoes-SsFGW3.png" )
 
-shoe = Shoe.create(name: "Nike Dunk Low By Talisha", brand: "Nike", size: "7 W" , price: 130, image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/78f55ed4-4d24-4938-8cdd-24e9ae908c3c/custom-nike-dunk-low-by-you-shoes.png" )
+# shoe = Shoe.create(name: "Nike Dunk Low By Talisha", brand: "Nike", size: "7 W" , price: 130, image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/78f55ed4-4d24-4938-8cdd-24e9ae908c3c/custom-nike-dunk-low-by-you-shoes.png" )
